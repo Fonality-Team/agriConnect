e67b510f85a3f1d282d3b65bbcad93d55285239b
Fon Godwill Ivan Tana <ivan8tana@gmail.com>
2025-06-26 07:24:12 +0100

    Add database initialization script

--- init_db.py
+++ init_db.py
@@ -1,6 +1,6 @@
 from app import create_app
 from app.extensions import db
-from app.models import all
+from app.models import *
 
 def init_db():
     """Initialize the database."""
@@ -8,3 +8,6 @@ def init_db():
     with app.app_context():
         db.create_all()  # Create all tables
         print("Database initialized successfully.")
+
+
+init_db()
